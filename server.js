9d25453f7c377f485587c284a9e60287096d7571
Vindhya Dulla <vindhyadulla@gmail.com>
2025-10-28 13:33:13 +0530

    Initial backend setup with Express server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+app.listen(5000, () => console.log("✅ Server running on port 5000"));
+
